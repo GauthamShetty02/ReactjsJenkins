a83de2f38bb87989dd5cd0e36a7fc5fd55dbb76a
Gautham Shetty <gauthamshetty@Gauthams-MacBook-Air.local>
2025-01-27 00:14:34 +0530

    Jenkin file

--- server/index.js
+++ server/index.js
@@ -1,11 +1,24 @@
 const express = require("express")
 const path = require("path")
 const app = express()
+const bunyan = require('bunyan');
 const port = process.env.PORT || 3001
+
+const log = bunyan.createLogger({
+    name: 'bunyan-best-practices',
+    serializers: bunyan.stdSerializers
+});
+
+
 
 app.use(express.static(path.join(__dirname, "../client/dist")))
 
 app.get("/api/hello", (req, res) => {
+    // Correct
+log.info({
+   
+    requestId: '268c3da8-a068-4c85-9a94-a845f8199f5f'
+}, 'Hello from the server!');
   res.json({ message: "Hello from the server!" })
 })
 
